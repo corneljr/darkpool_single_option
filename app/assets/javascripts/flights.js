0eab56f8dd373f4d1c128ebb68670c96205cee68
Josh Cornelius <joshcornelius@Joshs-MacBook-Air.local>
2016-12-01 16:12:51 -0500

    gmm

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -14,9 +14,9 @@ angular.module('flights', [])
         })
     };
 
-    this.tiers = [{'type':'whatever','title':'Flexible Super Saver','description':"Any flights +/- 1 day of your dates. Same trip length.", 'stops':'One stop or direct.', 'timeframe':'Anytime during the day','social':'26 people have bought this deal.'},
-      {'type':'anytype','title':'Anytime on your dates','description':'You will be booked on a nonstop or one-stop flight departing anytime on your dates.','stops':'One stop or direct','timeframe':'Anytime during the day', 'social':'15 people have bought this deal.'},
-      {'type':'anytime','title':'Go Direct','description':'Nonstop flights departing anytime during the day.','stops':'Direct','timeframe':'Anytime during the day','social':'19 people have bought this deal.'}
+    this.tiers = [{'type':'whatever','title':'Flexible Super Saver','description':"Any flights +/- 1 day of your dates. Same trip length.", 'stops':'One stop or nonstop.', 'timeframe':'Anytime during the day','social':'26 people have bought this deal.'},
+      {'type':'anytype','title':'Anytime on your dates','description':'You will be booked on a nonstop or one-stop flight departing anytime on your dates.','stops':'One stop or nonstop','timeframe':'Anytime during the day', 'social':'15 people have bought this deal.'},
+      {'type':'anytime','title':'Go Nonstop','description':'Nonstop flights departing anytime during the day.','stops':'Nonstop','timeframe':'Anytime during the day','social':'19 people have bought this deal.'}
     ]
 
     this.tierDetails = function(tier) {
