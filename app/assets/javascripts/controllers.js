6889559bf68f991320d0aa68c0a3b78f9eb08c15
Josh Cornelius <joshcornelius@Joshs-MacBook-Air.local>
2016-12-08 13:48:46 -0500

    boom

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -11,7 +11,7 @@ function ($scope, $stateParams, $timeout, $window, Flights, $state) {
 
     $scope.bunnyIndex = 1
     $scope.bunnyUrl = ''
-    $scope.expiryDate = new Date("2016-12-03")
+    $scope.expiryDate = new Date("2016-12-11")
     $scope.countdown = "";
 
     var dateTicker = function() {
